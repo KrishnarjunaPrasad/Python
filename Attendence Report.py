bf94fc467efbd1688dbc0322ce12aa45a25ad771
KrishnarjunaPrasad <92074715+KrishnarjunaPrasad@users.noreply.github.com>
2022-10-03 08:21:32 -0700

    Add files via upload

---
+++ Attendence Report.py
@@ -0,0 +1,11 @@
+print("Enter your PIN:")
+PIN= int(input())
+if PIN == 311 and 312 and 314 and 315:
+    print("attendence:75%")
+    if PIN ==  310 and 309 and 319:
+        print("Attendence :65%")
+else:
+    print("Go and meet HOD")
+
+
+      
---
+++ For loop.py
@@ -0,0 +1,11 @@
+kittu = ['kittu','prasaad','krishna','arjuna']
+for x in kittu:
+    print(x)
+    if x == 'prasaad':
+        continue
+    if x == 'krishna':
+         continue
+    print(x)
+else:
+    print("bye")
+    
